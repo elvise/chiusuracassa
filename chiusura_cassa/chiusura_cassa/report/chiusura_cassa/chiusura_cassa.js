97e9ecdefb23f750b529ebbdbd5864aea4215272
Syed Abdul Qadeer <sdqadeer44@gmail.com>
2018-01-19 11:32:14 +0300

    [Updated] - the report to show the data accordingly per store

--- chiusura_cassa/chiusura_cassa/report/chiusura_cassa/chiusura_cassa.js
+++ chiusura_cassa/chiusura_cassa/report/chiusura_cassa/chiusura_cassa.js
@@ -4,6 +4,25 @@
 
 frappe.query_reports["CHIUSURA CASSA"] = {
 	"filters": [
-
+		{
+			"fieldname":"store",
+			"label": __("Store"),
+			"fieldtype": "Link",
+			"options": "Store"
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("year_start_date")
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("year_end_date")
+		}
 	]
 }
--- chiusura_cassa/chiusura_cassa/report/chiusura_cassa/chiusura_cassa.py
+++ chiusura_cassa/chiusura_cassa/report/chiusura_cassa/chiusura_cassa.py
@@ -3,7 +3,46 @@
 
 from __future__ import unicode_literals
 import frappe
+from erpnext.controllers.queries import get_match_cond
+from frappe import _
+
 
 def execute(filters=None):
-	columns, data = [], []
+	if not filters: filters = frappe._dict()
+	columns, data = get_data(frappe._dict(filters))
+	return columns, data
+
+
+def get_data(filters):
+	conditions = ""
+	if filters.from_date:
+		conditions += " and `tabSales Invoice`.posting_date >= %(from_date)s"
+	if filters.to_date:
+		conditions += " and `tabSales Invoice`.posting_date <= %(to_date)s"
+	if filters.store:
+		conditions += " and `tabSales Invoice`.store = %(store)s"
+
+	columns = [
+		_("Store") + ":Link/Store:120",
+		_("Mode Of Payment") + "::120",
+		_("Grand Total") + "::120"]
+
+	if not filters.from_date and not filters.to_date:
+		frappe.throw(_("Please select From Date and To Date"))
+
+	data = frappe.db.sql("""
+		SELECT
+		`tabSales Invoice`.store,
+		`tabSales Invoice`.mode_of_payment,
+		SUM(`tabSales Invoice`.grand_total)
+
+	FROM
+		`tabSales Invoice`
+	WHERE
+		`tabSales Invoice`.docstatus = 1 {conditions} {match_cond}
+	GROUP BY `tabSales Invoice`.store, `tabSales Invoice`.mode_of_payment
+	ORDER BY
+		`tabSales Invoice`.posting_date desc, `tabSales Invoice`.posting_time desc
+	""".format(conditions=conditions, match_cond=get_match_cond('Sales Invoice')), filters)
+
 	return columns, data
